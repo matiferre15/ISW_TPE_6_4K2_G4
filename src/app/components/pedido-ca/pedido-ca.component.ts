5038061b920f6e1cd862910b3796c08cf3f45b89
matiferre15 <matias.ferre@hotmail.com>
2022-09-10 12:54:34 -0300

    Update interfaz grafica

--- src/app/components/pedido-ca/pedido-ca.component.ts
+++ src/app/components/pedido-ca/pedido-ca.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
   selector: 'app-pedido-ca',
   templateUrl: './pedido-ca.component.html',
   styleUrls: ['./pedido-ca.component.css'],
 })
 export class PedidoCaComponent implements OnInit {
+  
   FormPedidoCa = new FormGroup({
-    Calle: new FormControl(null),
-    Numero: new FormControl(null),
-    Ciudad: new FormControl(null),
+    Calle: new FormControl(null, [Validators.required, Validators.maxLength(35)]),
+    Numero: new FormControl(null, [Validators.pattern('[0-9]{1,5}'), Validators.required]),
+    Ciudad: new FormControl(null, [Validators.required, Validators.maxLength(35)]),
     Referencia: new FormControl(null),
     FormaPago: new FormControl(null),
     Monto: new FormControl(null),
@@ -21,10 +25,23 @@ export class PedidoCaComponent implements OnInit {
     Cvc: new FormControl(null),
   });
 
+  EstadoCarrito: string = "Lleno";
+
   Opciones = [{ Nombre: 'Efectivo' }, { Nombre: 'Tarjeta de credito' }];
+  
+
 
   constructor() {}
 
   ngOnInit() {}
+
+
+
+
+
+  alternarCarrito(){
+    this.EstadoCarrito == "Lleno" ? this.EstadoCarrito = "Vacio" : this.EstadoCarrito = "Lleno";
+    
+  }
 }
 
