5f6a00c3780ece56b9b80b357aaeb17158c4a791
matiferre15 <matias.ferre@hotmail.com>
2022-09-10 20:34:04 -0300

    Update validators

--- src/app/components/pedido-ca/pedido-ca.component.ts
+++ src/app/components/pedido-ca/pedido-ca.component.ts
@@ -14,30 +14,59 @@ export class PedidoCaComponent implements OnInit {
   FormPedidoCa = new FormGroup({
     Calle: new FormControl(null, [Validators.required, Validators.maxLength(35)]),
     Numero: new FormControl(null, [Validators.pattern('[0-9]{1,5}'), Validators.required]),
-    Ciudad: new FormControl(null, [Validators.required, Validators.maxLength(35)]),
-    Referencia: new FormControl(null),
-    FormaPago: new FormControl(null),
-    Monto: new FormControl(null),
-    NroTarjeta: new FormControl(null),
-    NombreTitular: new FormControl(null),
-    ApellidoTitular: new FormControl(null),
-    FechaVencimiento: new FormControl(null),
-    Cvc: new FormControl(null),
-  });
+    Ciudad: new FormControl('Córdoba'),
+    Referencia: new FormControl(null,[Validators.maxLength(240)]),
+    FormaPago: new FormControl('Tarjeta de credito'),})
+  //  Monto: new FormControl(null, [Validators.required,Validators.pattern('[0-9]{1,7}')]),
+  //  NroTarjeta: new FormControl(null, [Validators.required, Validators.pattern('[0-9]{16}')]),
+  //  NombreTitular: new FormControl(null,[Validators.required, Validators.maxLength(50)]),
+  //  ApellidoTitular: new FormControl(null,[Validators.required, Validators.maxLength(50)]),
+  //  FechaVencimiento: new FormControl(null,[Validators.required, 
+  //    Validators.pattern('(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}')
+  //  ]),
+  //  Cvc: new FormControl(null,[Validators.required,Validators.pattern('[0-9]{3,4}')]),
+  //});
 
-  EstadoCarrito: string = "Lleno";
+  FormPedidoCaEfectivo = new FormGroup({
+    Monto: new FormControl(null, [Validators.required,Validators.pattern('[0-9]{1,5}')]),
+  })
 
-  Opciones = [{ Nombre: 'Efectivo' }, { Nombre: 'Tarjeta de credito' }];
+  FormPedidoCaTarjeta = new FormGroup({
+    NroTarjeta: new FormControl(null, [Validators.required, Validators.pattern('[0-9]{16}')]),
+    NombreTitular: new FormControl(null,[Validators.required, Validators.maxLength(50)]),
+    ApellidoTitular: new FormControl(null,[Validators.required, Validators.maxLength(50)]),
+    FechaVencimiento: new FormControl(null,[Validators.required, 
+      Validators.pattern('(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}')
+    ]),
+    Cvc: new FormControl(null,[Validators.required,Validators.pattern('[0-9]{3,4}')]),
+  })
+
+  FormPedidoCaRecepcion = new FormGroup({
+    OpcionRecepcion: new FormControl('Lo antes posible'),
+    Fecha: new FormControl(null),
+    Hora: new FormControl(null),
+  })
+
+  EstadoCarrito = "Lleno";
+
+  Opciones = [{ Nombre: "Efectivo" }, { Nombre: "Tarjeta de credito" }];
+  OpcionesRecepcion = [{ Nombre: "Lo antes posible"}, { Nombre: "Elegir fecha y hora"}];
+  Ciudades = [{ Nombre: "Alta Gracia" }, { Nombre: "Arroyito" }, { Nombre: "Bell Ville" }, { Nombre: "Córdoba" },
+  { Nombre: "Cosquín" }, { Nombre: "Jesús María" }, { Nombre: "Marcos Juárez" }, { Nombre: "Río Cuarto" }, { Nombre: "Río Tercero" },
+  { Nombre: "San Francisco" }, { Nombre: "Villa Carlos Paz" }, { Nombre: "Villa María" }, { Nombre: "Villa Nueva" }];
+
+  Pedido = [{Nombre:"McCombo doble cuarto de libra", Imagen:"../../../assets/DobleCuartoDeLibra.png"}, 
+  { Nombre: "McCombo cuarto de libra", Imagen:"../../../assets/CuartoDeLibra.png"}, 
+  {Nombre: "Sundae dulce de leche", Imagen:"../../../assets/SundaeDdl.png"}];
+
+
+
   
 
 
   constructor() {}
 
-  ngOnInit() {}
-
-
-
-
+  ngOnInit() {  }
 
   alternarCarrito(){
     this.EstadoCarrito == "Lleno" ? this.EstadoCarrito = "Vacio" : this.EstadoCarrito = "Lleno";
